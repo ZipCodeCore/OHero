2d2d43a271ce30791dcf8547b19276cb8cfdb623
Mo Russ <mauriceruss302@gmail.com>
2020-03-14 19:38:21 -0400

    checkGameHealth stub make

--- src/Test/java/Games/ShaolimWarriorGameTest.java
+++ src/Test/java/Games/ShaolimWarriorGameTest.java
@@ -1,7 +1,12 @@
 package Games;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShaolimWarriorGameTest {
 
+    @Test
+    void checkGameHealth() {
+    }
 }
--- src/main/java/Games/ShaolimWarriorGame.java
+++ src/main/java/Games/ShaolimWarriorGame.java
@@ -1,4 +1,13 @@
 package Games;
 
 public class ShaolimWarriorGame {
+    private int enemyHealth = 100;
+    private double enemyMoveChoice = Math.ceil(Math.random()*3);
+    private String playerName = "playerHero.getName()";
+    private int playerHealth;
+
+
+    public void checkGameHealth(){
+
+    }
 }
