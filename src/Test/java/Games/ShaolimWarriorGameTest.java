77e216e4385a162e43eafeeede88eb6e45031e3f
Mo Russ <mauriceruss302@gmail.com>
2020-03-14 22:03:37 -0400

    checkHealthTest passed

--- src/Test/java/Games/ShaolimWarriorGameTest.java
+++ src/Test/java/Games/ShaolimWarriorGameTest.java
@@ -1,12 +1,26 @@
 package Games;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShaolimWarriorGameTest {
 
+
+    private static int enemyHealth = 100;
+    private static int playerHealth = 100;
+    private static boolean alive = false;
+
+
     @Test
     void checkGameHealth() {
+        if(enemyHealth <= 0)
+            alive = false;
+        else if (playerHealth <= 0)
+            alive = false;
+
+        Assert.assertTrue((  alive = true));
     }
 }
--- src/main/java/Games/ShaolimWarriorGame.java
+++ src/main/java/Games/ShaolimWarriorGame.java
@@ -2,12 +2,20 @@ package Games;
 
 public class ShaolimWarriorGame {
     private int enemyHealth = 100;
+    private String enemyName;
     private double enemyMoveChoice = Math.ceil(Math.random()*3);
     private String playerName = "playerHero.getName()";
     private int playerHealth;
+    private boolean alive = false;
 
 
     public void checkGameHealth(){
-
+        if (enemyHealth <= 0) {
+            System.out.println("Your training was tested and you chi is strong, " + playerName + " you have defeated " + enemyName + ".\n" + "This battle is over but there are more who must answer for your masters betrayal ");
+            alive = false;
+        } else if (playerHealth <= 0) {
+            System.out.println(playerName + " ,you have failed your master. " + enemyName + "'s chi was stronger. Master Li betrayal will not be avenged by you.");
+            alive = false;
+        }
     }
 }
