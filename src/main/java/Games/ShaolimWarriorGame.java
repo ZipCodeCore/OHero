69a8b337fef3f5777cebf52c5e749b52aaa9a514
Mo Russ <mauriceruss302@gmail.com>
2020-03-14 22:29:58 -0400

    game into

--- src/main/java/Games/ShaolimWarriorGame.java
+++ src/main/java/Games/ShaolimWarriorGame.java
@@ -1,5 +1,7 @@
 package Games;
 
+import output.Display;
+
 public class ShaolimWarriorGame {
     private int enemyHealth = 100;
     private String enemyName;
@@ -7,15 +9,27 @@ public class ShaolimWarriorGame {
     private String playerName = "playerHero.getName()";
     private int playerHealth;
     private boolean alive = false;
+    private Display gameDisplay = new Display();
 
+
+    public void ShaolinWarriorGameStart() {
+        gameDisplay.printMessage("There has been a travesty at the Shoalin monastery. \n" +
+                "Your master Huang Zongxi has been found slain. \n" +
+                "All signs show that it was the diciples of Wang Shichong. \n" +
+                "You have traveled to Kāimíng to exact your revenge for the the honor of your master");
+    }
 
     public void checkGameHealth(){
         if (enemyHealth <= 0) {
-            System.out.println("Your training was tested and you chi is strong, " + playerName + " you have defeated " + enemyName + ".\n" + "This battle is over but there are more who must answer for your masters betrayal ");
+            gameDisplay.printMessage("Your training was tested and you chi is strong, " + playerName + " you have defeated " + enemyName + ".\n" + "This battle is over but there are more who must answer for your masters betrayal ");
             alive = false;
         } else if (playerHealth <= 0) {
-            System.out.println(playerName + " ,you have failed your master. " + enemyName + "'s chi was stronger. Master Li betrayal will not be avenged by you.");
+            gameDisplay.printMessage(playerName + " ,you have failed your master. " + enemyName + "'s chi was stronger. Master Li betrayal will not be avenged by you.");
             alive = false;
         }
     }
+
+
+
+
 }
