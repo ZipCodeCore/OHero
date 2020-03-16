b1153a29755b5bc20f3dec350c209843faefbe09
Mo Russ <mauriceruss302@gmail.com>
2020-03-16 15:19:52 -0400

    enemyName issue fixed

--- src/main/java/Games/ShaolimWarriorGame.java
+++ src/main/java/Games/ShaolimWarriorGame.java
@@ -1,6 +1,7 @@
 package games;
 
 import input.ConsoleManager;
+import models.characters.Enemy;
 import models.characters.Hero;
 import models.items.Item;
 import models.utility.HeroWarehouse;
@@ -8,7 +9,7 @@ import output.Display;
 
 public class ShaolimWarriorGame {
     private static int enemyHealth = 100;
-    private static String enemyName = "Bolo";
+    private static Enemy badGuy = new Enemy(12345L,"Bolo","lackey");
     private static double enemyMoveChoice = Math.ceil(Math.random()*3);
     private static String playerName;
     private static int playerHealth;
@@ -28,10 +29,10 @@ public class ShaolimWarriorGame {
     public static void checkGameHealth(){
         //playerHealth = currentPlayer.getStats().getHealth();
         if (enemyHealth <= 0) {
-            gameDisplay.printMessage("Your training was tested and you chi is strong, " + playerName + " you have defeated " + enemyName + ".\n" + "This battle is over but there are more who must answer for your masters betrayal ");
+            gameDisplay.printMessage("Your training was tested and you chi is strong, " + playerName + " you have defeated " + badGuy.getName() + ".\n" + "This battle is over but there are more who must answer for your masters betrayal ");
             alive = false;
         } else if (playerHealth <= 0) {
-            gameDisplay.printMessage(playerName + " ,you have failed your master. " + enemyName + "'s chi was stronger. Master Huang betrayal will not be avenged by you.");
+            gameDisplay.printMessage(playerName + " ,you have failed your master. " + badGuy.getName() + "'s chi was stronger. Master Huang betrayal will not be avenged by you.");
             alive = false;
         }
     }
@@ -47,14 +48,14 @@ public class ShaolimWarriorGame {
                     "Your master Huang Zongxi has been found slain. \n" +
                     "All signs show that it was the disciples of Wang Shichong. \n" +
                     "You have traveled to Kāimíng to exact your revenge for the the honor of your master \n" +
-                    "You enter the enemies dojo." + enemyName + " is knealing in meditation. \n" +
+                    "You enter the enemies dojo. " + badGuy.getName() + " is knealing in meditation. \n" +
                     "He raises his head. You lock eyes and he rises to fight you.");
 
             alive = true;
         while (alive) {
 
 
-            gameDisplay.printMessage(enemyName + " has " + enemyHealth + " health remaining.");
+            gameDisplay.printMessage(badGuy.getName() + " has " + enemyHealth + " health remaining.");
             gameDisplay.printMessage(playerName + " has " + playerHealth + " health remaining.");
 
             gameDisplay.printMessage("What would you like to do? (a)punch (b)kick (c)knee strike: ");
@@ -72,17 +73,16 @@ public class ShaolimWarriorGame {
                 case "a" :
                     switch ((int) enemyChoice) {
                         case 1:
-                            gameDisplay.printMessage(enemyName + " side steps your punch." + enemyName + " is faster than you thought");
+                            gameDisplay.printMessage(badGuy.getName() + " side steps your punch." + badGuy.getName() + " is faster than you thought");
                             checkGameHealth();
                             break;
                         case 2:
-                            gameDisplay.printMessage(playerName + " lands a solid punch to " + enemyName + "'s ribs.");
-
+                            gameDisplay.printMessage(playerName + " lands a solid punch to " + badGuy.getName() + "'s ribs.");
                             enemyHealth -= 10;
                             checkGameHealth();
                             break;
                         case 3:
-                            gameDisplay.printMessage(enemyName + " sidesteps and counters with kick into " + playerName + ".");
+                            gameDisplay.printMessage(badGuy.getName() + " sidesteps and counters with kick into " + playerName + ".");
                             playerHealth -= 10;
                             checkGameHealth();
                             break;
@@ -92,16 +92,16 @@ public class ShaolimWarriorGame {
                 case "b":
                     switch ((int) enemyChoice) {
                         case 1:
-                            gameDisplay.printMessage(enemyName + " dances by kick.");
+                            gameDisplay.printMessage(badGuy.getName() + " dances by kick.");
                             checkGameHealth();
                             break;
                         case 2:
-                            gameDisplay.printMessage(playerName + " your kick doubles " + enemyName + " over.");
+                            gameDisplay.printMessage(playerName + " your kick doubles " + badGuy.getName() + " over.");
                             enemyHealth -= 10;
                             checkGameHealth();
                             break;
                         case 3:
-                            gameDisplay.printMessage(enemyName + " jumps back from your kick. And counters with a punch to " + playerName + "'s ribs.'");
+                            gameDisplay.printMessage(badGuy.getName() + " jumps back from your kick. And counters with a punch to " + playerName + "'s ribs.'");
                             playerHealth -= 10;
                             checkGameHealth();
                             break;
@@ -111,16 +111,16 @@ public class ShaolimWarriorGame {
                 case "c":
                     switch ((int) enemyChoice) {
                         case 1:
-                            gameDisplay.printMessage(enemyName + " laughs at your missed knee attack.");
+                            gameDisplay.printMessage(badGuy.getName() + " laughs at your missed knee attack.");
                             checkGameHealth();
                             break;
                         case 2:
-                            gameDisplay.printMessage(playerName + " your knee doubles " + enemyName + " over.");
+                            gameDisplay.printMessage(playerName + " your knee doubles " + badGuy.getName() + " over.");
                             enemyHealth -= 10;
                             checkGameHealth();
                             break;
                         case 3:
-                            gameDisplay.printMessage(enemyName + " evades your knee and lands a glancing blow to your face.");
+                            gameDisplay.printMessage(badGuy.getName() + " evades your knee and lands a glancing blow to your face.");
                             playerHealth -= 10;
                             checkGameHealth();
                             break;
